6c19ae596711763918efbb350f6a4c2d416de48a
marta <marta.gomez-barrero@h-da.de>
2017-12-22 15:45:25 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Unlinkability Metrics
+
+Implementation of the local and global unlinkability metrics for biometric template protection systems evaluation proposed in [TIFS17].
+
+## License
+This work is licensed under license agreement provided by Hochschule Darmstadt ([h_da-License](/hda-license.pdf)).
+
+## Instructions
+
+### Dependencies
+* seaborn
+* numpy
+* pylab
+* matplotlib
+* argparse
+
+### Usage
+
+1. Run evaluateUnlinkability.py 
+
+	```python
+	usage: evaluateUnlinkability.py [-h] [--omega [OMEGA]] [--nBins [NBINS]]
+									[--figureTitle [FIGURETITLE]]
+									[--legendLocation [LEGENDLOCATION]]
+									matedScoresFile nonMatedScoresFile figureFile
+
+	Evaluate unlinkability for two given sets of mated and non-mated linkage
+	scores.
+
+	positional arguments:
+	  matedScoresFile       filename for the mated scores
+	  nonMatedScoresFile    filename for the non-mated scores
+	  figureFile            filename for the output figure
+
+	optional arguments:
+	  -h, --help            show this help message and exit
+	  --omega [OMEGA]       omega value for the computations, if none provided,
+							omega = 1
+	  --nBins [NBINS]       number of bins for the computations, if none provided,
+							nBins = 100
+	  --figureTitle [FIGURETITLE]
+							title for the output figure
+	  --legendLocation [LEGENDLOCATION]
+							legend location
+	```
+
+2. Input: at least 3 score files (mated and non-mated score examples provided), and optionally other parameters of the computation and the formatting of the figure obtained as output.
+
+    The score files are loaded with the built-in function [numpy.fromfile()](https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.fromfile.html).
+    An example in hdf5 format has been provided, but other formats, such as a txt file with all scores separated by blank spaces
+    or one score per row, can be also used.
+
+3. Output: figure with score distributions, point-wise and global unlinkability metric results.
+
+## References
+
+More details in:
+
+- [[IS16](http://www.sciencedirect.com/science/article/pii/S0020025516304753)] M. Gomez-Barrero, C. Rathgeb, J. Galbally, C. Busch, J. Fierrez, "Unlinkable and Irreversible Biometric Template
+Protection based on Bloom Filters", in Elsevier Information Sciences, vol. 370-371, pp. 18-32, 2016
+
+- [TIFS17] M. Gomez-Barrero, J. Galbally, C. Rathgeb, C. Busch, "General Framework to Evaluate Unlinkability
+in Biometric Template Protection Systems", in IEEE Trans. on Informations Forensics and Security, 2017 (submitted)
+
+Please remember to reference articles [IS16] and [TIFS17] on any work made public, whatever the form,
+based directly or indirectly on these metrics.
---
+++ evaluateUnlinkability.py
@@ -0,0 +1,120 @@
+'''
+Implementation of the local and global unlinkability metrics for biometric template protection systems evaluation.
+More details in:
+
+[IS16] M. Gomez-Barrero, C. Rathgeb, J. Galbally, C. Busch, J. Fierrez, "Unlinkable and Irreversible Biometric Template
+Protection based on Bloom Filters", in Elsevier Information Sciences, vol. 370-371, pp. 18-32, 2016
+
+[TIFS17] M. Gomez-Barrero, J. Galbally, C. Rathgeb, C. Busch, "General Framework to Evaluate Unlinkability
+in Biometric Template Protection Systems", in IEEE Trans. on Informations Forensics and Security, 2017 (submitted)
+
+Please remember to reference articles [IS16] and [TIFS17] on any work made public, whatever the form,
+based directly or indirectly on these metrics.
+'''
+
+__author__ = "Marta Gomez-Barrero"
+__copyright__ = "Copyright (C) 2017 Hochschule Darmstadt"
+__license__ = "License Agreement provided by Hochschule Darmstadt (https://github.com/dasec/unlinkability-metric/blob/master/hda-license.pdf)"
+__version__ = "2.0"
+
+import numpy
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import pylab
+import seaborn as sns
+import argparse
+
+######################################################################
+### Parameter and arguments
+
+parser = argparse.ArgumentParser(description='Evaluate unlinkability for two given sets of mated and non-mated linkage scores.')
+parser.add_argument('matedScoresFile', help='filename for the mated scores', type=str)
+parser.add_argument('nonMatedScoresFile', help='filename for the non-mated scores', type=str)
+parser.add_argument('figureFile', help='filename for the output figure', type=str)
+parser.add_argument('--omega', help='omega value for the computations, if none provided, omega = 1', nargs='?', default=1., type=float)
+parser.add_argument('--nBins', help='number of bins for the computations, if none provided, nBins = 100', nargs='?', default=100, type=int)
+parser.add_argument('--figureTitle', help='title for the output figure', nargs='?', default='Unlinkability analysis', type=str)
+parser.add_argument('--legendLocation', help='legend location', nargs='?', default='upper right', type=str)
+
+
+args = parser.parse_args()
+matedScoresFile = args.matedScoresFile
+nonMatedScoresFile = args.nonMatedScoresFile
+figureFile = args.figureFile
+figureTitle = args.figureTitle
+legendLocation = args.legendLocation
+omega = args.omega
+nBins = args.nBins
+
+
+######################################################################
+### Evaluation
+
+# load scores
+matedScores = numpy.fromfile(matedScoresFile)
+nonMatedScores = numpy.fromfile(nonMatedScoresFile)
+
+# define range of scores to compute D
+bin_edges = numpy.linspace(min([min(matedScores), min(nonMatedScores)]), max([max(matedScores), max(nonMatedScores)]), num=nBins + 1, endpoint=True)
+bin_centers = (bin_edges[1:] + bin_edges[:-1]) / 2 # find bin centers
+
+# compute score distributions (normalised histogram)
+y1 = numpy.histogram(matedScores, bins = bin_edges, density = True)[0]
+y2 = numpy.histogram(nonMatedScores, bins = bin_edges, density = True)[0]
+
+# Compute LR and D
+LR = numpy.divide(y1, y2)
+D = 2*(omega*LR/(1 + omega*LR)) - 1
+D[omega*LR <= 1] = 0
+D[y2 == 0] = 1 # this is the definition of D, and at the same time takes care of inf / nan
+D[y1 == 0] = 0
+
+# Compute and print Dsys
+Dsys = numpy.trapz(x = bin_centers, y = D* y1)
+print(Dsys)
+
+
+### Plot final figure of D + score distributions
+
+plt.clf()
+
+sns.set_context("paper",font_scale=1.7, rc={"lines.linewidth": 2.5})
+sns.set_style("white")
+
+ax = sns.kdeplot(matedScores, shade=False, label='Mated', color=sns.xkcd_rgb["medium green"])
+x1,y1 = ax.get_lines()[0].get_data()
+ax = sns.kdeplot(nonMatedScores, shade=False, label='Non-Mated', color=sns.xkcd_rgb["pale red"],linewidth=5, linestyle='--')
+x2,y2 = ax.get_lines()[1].get_data()
+
+ax2 = ax.twinx()
+lns3, = ax2.plot(bin_centers, D, label='$\mathrm{D}_{\leftrightarrow}(s)$', color=sns.xkcd_rgb["denim blue"],linewidth=5)
+
+# print omega * LR = 1 lines
+index = numpy.where(D <= 0)
+ax.axvline(bin_centers[index[0][0]], color='k', linestyle='--')
+
+#index = numpy.where(LR > 1)
+#ax.axvline(bin_centers[index[0][2]], color='k', linestyle='--')
+#ax.axvline(bin_centers[index[0][-1]], color='k', linestyle='--')
+
+
+# Figure formatting
+ax.spines['top'].set_visible(False)
+ax.set_ylabel("Probability Density")
+ax.set_xlabel("Score")
+ax.set_title("%s, $\mathrm{D}_{\leftrightarrow}^{\mathit{sys}}$ = %.2f" % (figureTitle, Dsys),  y = 1.02)
+
+labs = [ax.get_lines()[0].get_label(), ax.get_lines()[1].get_label(), ax2.get_lines()[0].get_label()]
+lns = [ax.get_lines()[0], ax.get_lines()[1], lns3]
+ax.legend(lns, labs, loc = legendLocation)
+
+ax.set_ylim([0, max(max(y1), max(y2)) * 1.05])
+ax.set_xlim([bin_edges[0]*0.98, bin_edges[-1]*1.02])
+ax2.set_ylim([0, 1.1])
+ax2.set_ylabel("$\mathrm{D}_{\leftrightarrow}(s)$")
+
+plt.gcf().subplots_adjust(bottom=0.15)
+plt.gcf().subplots_adjust(left=0.15)
+plt.gcf().subplots_adjust(right=0.88)
+pylab.savefig(figureFile)
